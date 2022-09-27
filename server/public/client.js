4a9214a3af07b6c0049744e38871ef1da9779e29
Edan Schwartz <edanschwartz@gmail.com>
2022-09-27 16:59:58 -0500

    Lecture code

--- server/public/client.js
+++ server/public/client.js
@@ -1,1 +1,23 @@
 console.log('in client.js 🎀');
+
+/* 
+$(document).ready(() => {
+    let state = goGrabDataFromTheServer();
+
+    render();
+});
+
+function onUpvote() {
+    sendToTheServer({
+        totalVotes: 51
+    });
+
+    render()
+}
+
+function render() {
+    for (let post of state.posts) {
+
+    }
+}
+ */
--- server/server.js
+++ server/server.js
@@ -2,9 +2,32 @@ const express = require('express');
 
 const app = express();
 
+let comments = [
+    'Servers are neat',
+    'Well akhshully....',
+    'narwhal bacon something something... undertaker...'
+];
+
 // Make files in the server/public folder
 // available to who wants them
 app.use(express.static('./server/public'));
+
+// Like an "Event Handler"
+// When Jimmy asks for comments
+// http://localhost:3000/comments
+// call this function
+//
+app.get('/comments', (req, res) => {
+    console.log('Jimmy wants those comments!');
+
+    res.send(comments);
+});
+
+app.get('/comments/first', (req, res) => {
+    console.log('in /comments/first');
+
+    res.send(comments[0]);
+});
 
 app.listen(3000, () => {
     console.log('We\'re live! 🎰');
